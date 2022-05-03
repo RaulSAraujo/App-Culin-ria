091e00ddfca458ea49d0f616ea5bc8fd58c3bf86
Raul Silva Araujo <raul.araujo.2000@hotmail.com>
2022-05-03 17:27:34 -0300

    Inicio

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'screens/categories_screen.dart';
+import './screens/categories_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
+import 'Utils/app_routes.dart';
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleSmall: TextStyle(
+            fontSize: 14,
+            fontFamily: 'Raleway',
+          ),
+          titleMedium: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+          ),
+          titleLarge: TextStyle(
+            fontSize: 26,
+            fontFamily: 'RobotoCondensed',
+          )
+        )
+      ),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS:(ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL:(ctx) => MealDetailScreen(),
+      },
+    );
+  }
+}
+ 
