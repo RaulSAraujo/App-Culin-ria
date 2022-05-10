3e219f1cfbf28dee03015e3843fe3d9188946d21
Raul Silva Araujo <raul.araujo.2000@hotmail.com>
2022-05-10 10:04:24 -0300

    Adicionando Drawer na AppBar
    Adicionando Drawer na AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,39 @@ import 'screens/categories_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
 import 'Utils/app_routes.dart';
+import './screens/settings_screen.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          titleSmall: TextStyle(
-            fontSize: 12,
-            fontFamily: 'Raleway',
-          ),
-          titleMedium: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-          ),
-          titleLarge: TextStyle(
-            fontSize: 26,
-            fontFamily: 'RobotoCondensed',
-          )
-        )
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+              titleSmall: TextStyle(
+                fontSize: 12,
+                fontFamily: 'Raleway',
+              ),
+              titleMedium: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+              titleLarge: TextStyle(
+                fontSize: 26,
+                fontFamily: 'RobotoCondensed',
+              ))),
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS:(ctx) => CategoriesMealsScreen(),
-        AppRoutes.MEAL_DETAIL:(ctx) => MealDetailScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
     );
   }
 }
- 
